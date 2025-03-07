726233613c35c8c8c016d423b9754d76811197ff
Ahmed-Wael-01 <xahmedwaheed@gmail.com>
2025-03-07 15:13:10 -0800

    three

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """basic flask app"""
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from flask_babel import Babel
 
 
